c931cc57b70728e3426dd847bf8df6f57c159d14
Mayi <caballeskatrina@gmail.com>
2024-11-11 15:04:20 +0800

    login added

--- coffito/src/App.jsx
+++ coffito/src/App.jsx
@@ -1,5 +1,5 @@
 // App.jsx
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Dashboard from "./Dashboard";
@@ -9,15 +9,28 @@ import SalesReportMonthly from "./SalesReportMonthly";
 import SalesReportYearly from "./SalesReportYearly";
 import Transaction from "./Transaction";
 import ItemSold from "./ItemSold";
+import Login from "./Login";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const handleLogin = () => {
+    setIsAuthenticated(true);
+  };
+
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+  };
+
   return (
     <Router>
       <div className="flex">
-        <Sidebar />
+        {isAuthenticated && <Sidebar onLogout = {handleLogout}/>}
 
         <div className="flex-grow">
           <Routes>
+            <Route path="/" element={isAuthenticated ? <Dashboard /> : <Login onLogin={handleLogin} />} />
+
             <Route path="/" element={<Dashboard />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/product" element={<Product />} />
